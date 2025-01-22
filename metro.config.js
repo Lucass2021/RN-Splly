aae1f93cb24441efa9a216da22ec2e26d97e6f58
Lucas Dias <lucas.allx@hotmail.com>
2025-01-22 19:16:30 -0300

    chore: rebuild project initial

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
-
-module.exports = (() => {
-    const config = getDefaultConfig(__dirname);
-
-    // Configuração do nativewind
-    return withNativeWind(config, { input: './src/global.css' });
-})();
