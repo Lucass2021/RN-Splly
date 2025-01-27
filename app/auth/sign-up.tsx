9cc5c5b07053c2cf8cf53621dbe9d9048a6af18d
Lucas Dias <lucas.allx@hotmail.com>
2025-01-26 21:43:32 -0300

    feat: sign-up screen initial

--- app/auth/sign-up.tsx
+++ app/auth/sign-up.tsx
@@ -1,9 +1,28 @@
+import Button from "@/components/button/button";
 import {Text, View} from "react-native";
 
 export default function SignUp() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Sign Up</Text>
+    <View className="flex-1 px-7.5 justify-center">
+      <View>
+        <Text className="text-center color-warningOne text-3xl font-obviouslyBold mb-5">
+          Crie sua conta
+        </Text>
+        <Text className="text-center color-grayOne text-base font-TTInterphasesRegular">
+          Lorem ipsum potenti orci suspendisse aliquam nullam ornare dictumst
+          id, dictumst risus viverra.
+        </Text>
+      </View>
+
+      <View></View>
+
+      <View className="mt-12">
+        <Button text="Criar Conta" onPress={() => {}} />
+        <Text className="mt-3 text-sm text-center font-TTInterphasesRegular color-grayTwo">
+          Já tem uma conta?{" "}
+          <Text className="color-grayThree underline">Fazer Login</Text>
+        </Text>
+      </View>
     </View>
   );
 }
