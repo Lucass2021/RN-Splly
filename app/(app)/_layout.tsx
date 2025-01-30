6cd5bd09531199df2b09b0fa755861c2edf8a20a
Lucas Dias <lucas.allx@hotmail.com>
2025-01-30 19:17:35 -0300

    fix: TODO update

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -3,8 +3,8 @@ import {Stack} from "expo-router";
 
 export default function AppLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(app)" options={{headerShown: false}} />
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="index" options={{headerShown: false}} />
     </Stack>
   );
 }
