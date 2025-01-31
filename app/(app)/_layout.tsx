fe709f543f9c9cdc118890e1eb484a975e0d4e8e
Lucas Dias <lucas.allx@hotmail.com>
2025-01-31 13:18:34 -0300

    feat: initial tab bar menu

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,101 @@
-// app/(app)/layout.tsx
-import {Stack} from "expo-router";
+import {FontAwesome} from "@expo/vector-icons";
+import {Tabs} from "expo-router";
+import {Text} from "react-native";
 
 export default function AppLayout() {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name="index" options={{headerShown: false}} />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          paddingTop: 10,
+          height: 80,
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <FontAwesome name="list-ul" className="text-accent text-2xl" />
+              );
+            }
+            return (
+              <FontAwesome name="list-ul" className="text-body text-2xl" />
+            );
+          },
+          tabBarLabel: ({focused}) => (
+            <Text
+              className={focused ? "text-secondaryVariantOne" : "text-grayOne"}>
+              Home
+            </Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <FontAwesome name="list-ul" className="text-accent text-2xl" />
+              );
+            }
+            return (
+              <FontAwesome name="list-ul" className="text-body text-2xl" />
+            );
+          },
+          tabBarLabel: ({focused}) => (
+            <Text
+              className={focused ? "text-secondaryVariantOne" : "text-grayOne"}>
+              Explorar
+            </Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="schedule"
+        options={{
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <FontAwesome name="list-ul" className="text-accent text-2xl" />
+              );
+            }
+            return (
+              <FontAwesome name="list-ul" className="text-body text-2xl" />
+            );
+          },
+          tabBarLabel: ({focused}) => (
+            <Text
+              className={focused ? "text-secondaryVariantOne" : "text-grayOne"}>
+              Agenda
+            </Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <FontAwesome name="list-ul" className="text-accent text-2xl" />
+              );
+            }
+            return (
+              <FontAwesome name="list-ul" className="text-body text-2xl" />
+            );
+          },
+          tabBarLabel: ({focused}) => (
+            <Text
+              className={focused ? "text-secondaryVariantOne" : "text-grayOne"}>
+              Perfil
+            </Text>
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
