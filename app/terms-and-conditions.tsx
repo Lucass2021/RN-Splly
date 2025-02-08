7d7dbb9b1c26cd5c509d6a83473c024b8c4c51b8
Lucas Dias <lucas.allx@hotmail.com>
2025-02-08 11:08:17 -0300

    fix: added background and shadow for accept terms and condition button

--- app/terms-and-conditions.tsx
+++ app/terms-and-conditions.tsx
@@ -71,25 +71,33 @@ export default function TermsAndConditions() {
   const {setUserTermsAndConditions} = useAuthActions();
   const userTermsAndConditions = useUserTermsAndConditions();
 
+  const customShadow = {
+    shadowColor: "#000000", // Android and IOS
+    shadowOffset: {width: 0, height: -4}, // IOS Only
+    shadowOpacity: 0.05, // IOS Only
+    shadowRadius: 6.7, // IOS Only
+    elevation: 5, // Android only
+  };
+
   const handleAcceptTermsAndConditions = () => {
     setUserTermsAndConditions(true);
     router.push("/auth/sign-up");
   };
 
   return (
-    <View className="flex-1 px-7.5 pt-13 bg-light">
-      <Topic topicData={topicData} />
-      <View className="position-absolute bottom-0">
-        {!userTermsAndConditions && (
-          <View className="pb-10">
-            <Button
-              text="Aceitar"
-              onPress={handleAcceptTermsAndConditions}
-              disabled={userTermsAndConditions}
-            />
-          </View>
-        )}
-      </View>
+    <View className="flex-1 pt-13 bg-light">
+      <Topic topicData={topicData} customClassName="px-7.5" />
+      {!userTermsAndConditions && (
+        <View
+          className="pt-2.5 pb-5 px-7.5 bg-light rounded-t-2xl"
+          style={customShadow}>
+          <Button
+            text="Aceitar"
+            onPress={handleAcceptTermsAndConditions}
+            disabled={userTermsAndConditions}
+          />
+        </View>
+      )}
     </View>
   );
 }
