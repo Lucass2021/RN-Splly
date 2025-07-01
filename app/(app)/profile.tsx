ba0fa2c290a7bb984711ea1e4b25fc4cf8258725
Lucas Dias <lucas.allx@hotmail.com>
2025-07-01 20:15:14 -0300

    feat: initial profile screen

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,10 +1,13 @@
-import Button from "@/components/button/button";
+import ArrowDownComponent from "@/assets/icons/arrowDown";
+import TextComponent from "@/components/text/text";
 import {useAuthActions} from "@/store/auth";
+import {Colors} from "@/theme/colors";
 import {router} from "expo-router";
-import {Text, View} from "react-native";
+import {Image, Platform, Pressable, ScrollView, View} from "react-native";
 
 export default function ProfileScreen() {
   const {logout} = useAuthActions();
+  const userDemo = require("../../src/assets/images/user.png");
 
   const handleUserLogout = () => {
     logout();
@@ -12,13 +15,52 @@ export default function ProfileScreen() {
   };
 
   return (
-    <View className="flex-1 justify-center items-center px-7.5">
-      <View className="w-full">
-        <Text className="text-center mb-5 font-obviouslySemiBold">
-          Profile App Route
-        </Text>
-        <Button text="Logout" onPress={handleUserLogout} />
+    <ScrollView
+      className="flex-1 pt-13 bg-light"
+      contentContainerStyle={{paddingBottom: Platform.OS === "ios" ? 50 : 100}}>
+      <View className="px-7.5">
+        <Pressable onPress={handleUserLogout}>
+          <View className="justify-center items-center">
+            <TextComponent
+              fontFamily="Obviously"
+              fontWeight="SemiBold"
+              color="dark"
+              fontSize="h6">
+              Perfil
+            </TextComponent>
+          </View>
+        </Pressable>
+
+        <View className="mt-5 bg-secondaryVariantThree rounded-2xl p-3 flex-row items-center gap-3.5">
+          <Image source={userDemo} className="rounded-2xl" resizeMode="cover" />
+
+          <View>
+            <TextComponent
+              fontFamily="TTInterphases"
+              fontWeight="Bold"
+              color="lightOne"
+              fontSize="h6">
+              Gabriel de Mello
+            </TextComponent>
+            <View className="flex-row items-center gap-x-2">
+              <TextComponent
+                fontFamily="Obviously"
+                fontWeight="Medium"
+                color="grayNine"
+                fontSize="caption">
+                Editar Detalhes
+              </TextComponent>
+              <View className="-rotate-90">
+                <ArrowDownComponent
+                  color={Colors.grayNine}
+                  width={15}
+                  height={20}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
